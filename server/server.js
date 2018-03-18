296dc3c7b723d2b8c15c30ca9fdc42fd60dd1d70
Ben Keating <ben.l.keating@gmail.com>
2018-03-18 21:55:40 +0000

    =greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,8 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'new user joined.',
+    createdAt: new Date().getTime()
   });
 
 
@@ -27,8 +35,18 @@ io.on('connection', (socket) => {
       text: newMessage.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createdAt: new Date().getTime()
+    // })
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
+
 
 
 server.listen(port, () => {
