fe714388514de007ed76be03410e57cc2c305785
Ben Keating <ben.l.keating@gmail.com>
2018-03-19 21:46:22 +0000

    =add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message.js');
+const {generateMessage, generateLocationMessage} = require('./message.js');
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
     var from = 'Ben';
@@ -14,4 +14,19 @@ describe('generateMessage', () => {
     //assert createdAt //typeof toBe
     expect(typeof res.createdAt).toBe('number');
   });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Ben';
+    var latitude = 1;
+    var longitude = 2;
+
+    var res = generateLocationMessage(from, latitude, longitude);
+
+    expect(res.from).toBe(from);
+
+    expect(res.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`);
+  });
+
 });
